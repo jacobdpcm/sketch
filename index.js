2974589bc2a3f6abc6b1c74839a976fd36022875
JacobPCM <jacobd@postcardmania.com>
2023-09-21 17:34:04 -0400

    Establish initial framework of site and grid.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+//Create a grid with specific dimensions
+function gridCreate(){
+    let squares = 256;
+    const grid = document.querySelector('.grid');
+    for(let i = 0; i < squares; i++){
+        const newSquare = document.createElement('div');
+        newSquare.classList.add('square');
+        grid.appendChild(newSquare);
+    }
+}
 
+gridCreate()
